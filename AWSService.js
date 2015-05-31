16c60256e16ebf9f8044884854ed4c0815b2c354
= <gruber.asher@post.idc.ac.il>
2015-05-31 14:43:54 +0300

    Support get item in batch. Refactor.

--- AWSService.js
+++ AWSService.js
@@ -1,102 +1,10 @@
-/**
- * Created by asher on 07/04/15.
- */
 module.exports = function () {
     var Promise = require('bluebird');
     var AWS = require('aws-sdk');
-    const bucketName = 'cloudcomp.agruber';
 
     function setup() {
         // The AWS Credentials are loaded from the credential local file which is defined as best practice.
         AWS.config.region = 'eu-west-1';
-    }
-
-    function getFile(key) {
-        return new Promise(function(resolve, reject){
-            var params = {Bucket: bucketName, Key: key, ResponseContentType : 'application/json'};
-            var s3 = new AWS.S3();
-            s3.getObject(params, function(error, data) {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-
-    function getFiles() {
-        return new Promise(function (resolve, reject) {
-            var params = {Bucket: bucketName};
-            var s3 = new AWS.S3();
-            s3.listObjects(params, function (error, data) {
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-
-    function uploadFile(key, filepath) {
-        var fs = require('fs');
-        var stat = Promise.promisify(fs.stat);
-
-        return Promise.bind(this)
-            .then(function() { return stat(filepath); })
-            .then(function(fileInfo) {
-                var bodyStream = fs.createReadStream(filepath);
-                return putObject(bucketName, key, bodyStream, fileInfo.size);
-            });
-    }
-
-    function putObject (bucket, key, body, contentLength) {
-        return new Promise(function(resolve, reject){
-            var params = {
-                Bucket: bucket,
-                Key: key,
-                Body: body,
-                ContentLength: contentLength
-            };
-
-            var s3 = new AWS.S3();
-            s3.putObject(params, function(error, data) {
-                if (error) { reject(error); }
-                else { resolve(data); }
-            });
-        });
-    }
-
-    function getEC2InstanceData() {
-        return new Promise(function(resolve, reject){
-            var ec2 = new AWS.EC2();
-            ec2.describeInstances(function(err, data) {
-                if (err) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
-    }
-
-    function getELBInstanceData() {
-        return new Promise(function(resolve, reject){
-            var elb = new AWS.ELB();
-
-            elb.describeLoadBalancers(function(err, data) {
-                if (err) {
-                    reject(error);
-                }
-                else {
-                    resolve(data);
-                }
-            });
-        });
     }
 
     function getItem(key) {
@@ -126,16 +34,19 @@ module.exports = function () {
 
     function batchGetItem(keys) {
         return new Promise(function(resolve, reject) {
+            // Define the basic params object.
             var params = {
                 RequestItems : {
-                    "InvertedIndex" : {
-                        Keys : [
-                            {"Word" : { "S" : keys[0] } },
-                            {"Word" : { "S" : keys[1] } }
-                        ]
+                    InvertedIndex : {
+                        Keys : []
                     }
                 }
             }
+
+            // Fill keys according to the given input.
+            keys.forEach(function(key) {
+               params.RequestItems.InvertedIndex.Keys.push({ Word : { S : key } });
+            });
 
             var db = new AWS.DynamoDB();
             db.batchGetItem(params, function(err, data) {
@@ -153,11 +64,6 @@ module.exports = function () {
 
     return {
         setup : setup,
-        getFile : getFile,
-        getFiles : getFiles,
-        uploadFile : uploadFile,
-        getEC2InstanceData : getEC2InstanceData,
-        getELBInstanceData : getELBInstanceData,
         getItem : getItem,
         batchGetItem : batchGetItem
     };
