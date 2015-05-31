8e56641efbafa4b0e778b3ecda0110b811e28847
= <gruber.asher@post.idc.ac.il>
2015-05-31 13:56:11 +0300

    Search dynamoDB for key.

--- routes/aws.js
+++ routes/aws.js
@@ -7,7 +7,7 @@ var s3 = require('../AWSService')();
 s3.setup();
 
 exports.search = function (req, res) {
-    var searchKey = req.query.word;
+    var searchKey = req.query.searchTerm;
     s3.getItem(searchKey).then(function(data) {
         res.send(data);
     });
