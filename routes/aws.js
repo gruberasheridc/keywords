979fd6c615022c78b178546d44da442c018575e3
= <gruber.asher@post.idc.ac.il>
2015-06-01 19:27:51 +0300

    Support the sites presentation in the UI.

--- routes/aws.js
+++ routes/aws.js
@@ -11,15 +11,20 @@ exports.search = function (req, res) {
 };
 
 exports.query = function (req, res) {
+    var websites = [];
     var searchKey = req.query.searchTerm;
-    db.queryItems(searchKey).then(function(data) {
-        // Process data and send response to the caller.
-        var websites = [];
-        data.Items.forEach(function(item) {
-            var website = item.Url.S;
-            websites.push(website);
-        })
+    if (searchKey) {
+        db.queryItems(searchKey).then(function(data) {
+            // Process data and send response to the caller.
+            data.Items.forEach(function(item) {
+                var website = item.Url.S;
+                websites.push(website);
+            })
 
+            res.send(websites);
+        });
+    } else {
+        // No search term was found. Return an empty websites list.
         res.send(websites);
-    });
+    }
 };
