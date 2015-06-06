65139cc511019a7a3139bb6f4f7b1cf109073ef9
= <gruber.asher@post.idc.ac.il>
2015-06-06 12:44:22 +0300

    Insert data to DynamoDB backup.

--- routes/aws.js
+++ routes/aws.js
@@ -50,18 +50,68 @@ exports.fillTalbe = function (req, res) {
                 var wordSites = spliteWordSites(line);
                 var word = wordSites[0];
                 var sites = wordSites[1]; // If the word is a URL sites will contain a number.
+                wordSitesMap.set(word, sites);
             });
 
-            // Generate items for insert. Each item contains Word, Url, Rank.
-            var items = [];
+            // Generate items for insert to the InvertedIndex Table (item contains Word, Sites).
+            var invertedIndexItems = [];
+            wordSitesMap.entries().forEach(function(entry) {
+                var word = entry[0];
+                var sites = entry[1];
+
+                var item = {
+                    Item: {
+                        Word: {
+                            S: word
+                        },
+                        sites: {
+                            S: sites
+                        }
+                    }
+                };
+
+                invertedIndexItems.push(item);
+            });
+
+            // Generate items for insert to the WordUrlRank. Each item contains Word, Url, Rank.
+            // We will access the inverted index table only for getting the rank of URLs (it is possible that some ranks
+            // exist in the Inverted Index table that are not in the map (if this is a case of update).
+            var rankItems = [];
             wordSitesMap.entries().forEach(function(entry) {
                 var word = entry[0];
                 if (validator.isURL(word)) {
-                    // Urls words are only
+                    // Urls words are only inserted as keys to the InvertedIndex table.
+                    // We will perform a lookup to the InvertedIndex table for getting their rank.
                     return;
                 }
 
-                var sites = entry[1];
+                var sites = entry[1]; // We get the sites from the map.
+                var sitesList = sites.split(",");
+                sitesList.forEach(function(site) {
+                    // Generate an item for every Word, Site, Site Rank.
+                    var siteRank = wordSitesMap.get(site)
+                    if (!siteRank) {
+                        // If site is not in map.
+                        siteRank = 0;
+                    }
+
+                    var item = {
+                        Item: {
+                            Word: {
+                                S: word
+                            },
+                            Url: {
+                                S: site
+                            },
+                            Rank: {
+                                N: siteRank
+                            }
+                        }
+                    };
+
+                    rankItems.push(item);
+                });
+
             });
         }
 /*        db.batchWriteItem().then(function(data) {
